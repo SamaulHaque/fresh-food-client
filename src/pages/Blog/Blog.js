e1557627581782fad8a2ab5f4616cc9412f7e5a8
SamaulHaque <samaulhaque749@gmail.com>
2022-11-15 15:32:02 +0600

    footer added

--- src/pages/Blog/Blog.js
+++ src/pages/Blog/Blog.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h2></h2>
+            <h2 className='text-3xl font-semibold'>Difference between SQL and NoSQL</h2>
+            <p><b>SQL: </b> SQL or the Structured Query Language is the most common and popular programming language for the relational database management system. It is a language designed to extract, store, insert, delete, update and manage data for structured data and strategic analysis. SQL is widely used by top tech companies like Facebook, Whatsapp, etc for data processing solutions. It is used for different types of RDBMS including Oracle, MySQL, SQLServer, etc. 
+
+</p>
         </div>
     );
 };
