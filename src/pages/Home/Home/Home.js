28f643f666d8336c9ac40e30783ec8fa1ebc77fd
SamaulHaque <samaulhaque749@gmail.com>
2022-11-15 16:38:39 +0600

    food gallery section added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FoodGallery from '../../FoodGallery/FoodGallery';
 import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div className='mb-6'>
             <Banner></Banner>
+            <FoodGallery></FoodGallery>
         </div>
     );
 };
