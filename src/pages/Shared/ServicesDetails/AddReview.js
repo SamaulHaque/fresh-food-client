16f7d134852a021b3d3fe324886091a58009b3fd
SamaulHaque <samaulhaque749@gmail.com>
2022-11-16 19:32:24 +0600

    save reviews data in db using post

--- src/pages/Shared/ServicesDetails/AddReview.js
+++ src/pages/Shared/ServicesDetails/AddReview.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 
 const AddReview = () => {
+
+    const {user} = useContext(AuthContext);
+
+    const handelAddReview = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = user?.displayName || form.name.value;
+        const email = user?.email || 'unregister';
+        const image = user?.photoURL || form.image.value;
+        const message = form.message.value;
+
+        const review = {
+            name,
+            email,
+            image,
+            message
+        }
+
+        fetch('http://localhost:5000/reviews', {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify(review)  
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.acknowledged > 0){
+                form.reset();
+            }
+        })
+        .catch(error => console.log(error))
+
+    }
+
     return (
         <div className='mt-12'>
             <h2 className='text-center text-3xl font-bold mb-6'>Please Add Your Review.</h2>
-         <form>
-            <input type="text" name='name' placeholder="name" className="input input-bordered w-full mb-3" />
-            <input type="text" name='image' placeholder="img url" className="input input-bordered w-full mb-3" />
-            <textarea name='message' className="textarea textarea-bordered w-full mb-3" placeholder="Add Your Review"></textarea>
+         <form onSubmit={handelAddReview}>
+            <input type="text" name='name' defaultValue={user?.displayName} placeholder="Your Name" className="input input-bordered w-full mb-3" required/>
+            <input type="email" name='email' readOnly defaultValue={user?.email} placeholder="Your Email" className="input input-bordered w-full mb-3" required/>
+            <input type="text" name='image' defaultValue={user?.photoURL} placeholder="Image URL" className="input input-bordered w-full mb-3" required/>
+            <textarea name='message' className="textarea textarea-bordered w-full mb-3" placeholder="Please Type Your Review here" required></textarea>
 
-            <button className='btn btn-outline'>Submit Review</button>  
+            <input  type="submit" value='Submit Review' className='btn btn-outline' /> 
          </form>
         </div>
     );
